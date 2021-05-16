53e57b459f283bc33ffb9610d2748d2c6866526c
Neradoc <neraOnGit@ri1.fr>
2021-05-16 04:24:49 +0200

    serial exchange with input() updated to slightly generic neopixel/buttons code

--- serial_exchange/code-serial_to_neopixel.py
+++ serial_exchange/code-serial_to_neopixel.py
@@ -1,42 +1,100 @@
 import board
 import digitalio
 import json
-import neopixel
 import time
-import re
 import supervisor
 
-# echo -e "0,50,50\r" > /dev/cu.usbmodem144443301
+################################################################
+# init board's LEDs for visual output
+# replace with your own pins and stuff
+################################################################
 
-pix = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pix[0] = (255, 128, 0)
+pix = None
+if hasattr(board,"NEOPIXEL"):
+    import neopixel
+    pix = neopixel.NeoPixel(board.NEOPIXEL, 1)
+    pix.fill((32, 16, 0))
 
-if hasattr(board, "BUTTON"):  # QTPY 2040 button or other
-    button = digitalio.DigitalInOut(board.BUTTON)
+led = None
+if hasattr(board,"LED"):
+    led = digitalio.DigitalInOut(board.LED)
+    led.switch_to_output()
+    led.value = False
+
+################################################################
+# init board's button for acknowledging user interaction
+# replace with your own pins and stuff
+# - the code tries its best to find a default button
+# - two fixed default values on some boards (for my tests)
+################################################################
+
+# boards with buttons:
+BUTTONS_CANDIDATES = ["BUTTON", "BUTTON_USR", "BUTTON_USER", "BUTTON_A", "BUTTON_X", "BUTTON_UP", "BUTTON1", "BUTTON_1"]
+for btn_pin in BUTTONS_CANDIDATES:
+    if hasattr(board, btn_pin):
+        button = digitalio.DigitalInOut(getattr(board, btn_pin))
+        button.switch_to_input(digitalio.Pull.UP)
+        button_id = btn_pin
+        break
+else: # no break
+    """
+    Change the BUTTON pin to match your setup, and button_id
+    """
+    # this is an example for the pico
+    if hasattr(board,"GP3"): pin = board.GP3
+    # this is an example for most boards/feathers
+    elif hasattr(board,"A2"): pin = board.A2
+    # pin = board.SOMEPIN
+    button = digitalio.DigitalInOut(pin)
     button.switch_to_input(digitalio.Pull.UP)
-else:
-    button = None
+    button_id = repr(pin).replace("board.","")
 
-pattern = re.compile("(\d+),(\d+),(\d+)")
-button_past = button.value
+################################################################
+# prepare values for the loop
+################################################################
+
+if button:
+    button_past = button.value
+
+################################################################
+# loop-y-loop
+################################################################
+
 while True:
     data_out = {}
-    if supervisor.runtime.serial_bytes_available:
-        x = input()
-        try:
-            data = json.loads(x)
-        except:
-            data = {"raw": x}
-        #
-        if "color" in data:
-            pix[0] = data["color"]
-            # print("Color set:",color)
+
+    # read the secondary serial line by line
+    # when there's data, with a timeout
+    if supervisor.runtime.serial_bytes_available > 0:
+        data_in = input()
+
+        # try to convert the data to a dict (with JSON)
+        data = None
+        if len(data_in) > 0:
+            try:
+                data = json.loads(data_in)
+            except:
+                data = {"raw": data_in}
+
+        # interpret 
+        if isinstance(data,dict):
+            if "color" in data and pix is not None:
+                pix.fill(data["color"])
+            if "blink" in data and led is not None:
+                # blinking without sleep is left as an exercise
+                led.value = True
+                time.sleep(0.25)
+                led.value = False
+                time.sleep(0.25)
+
+    # read the buttons and send the info to the serial
     if button and button_past != button.value:
         button_past = button.value
         if not button.value:
-            data_out["buttons"] = [{"status": "PRESSED", "id": "BOOT"}]
+            data_out["buttons"] = [{"status": "PRESSED", "id": button_id}]
         else:
-            data_out["buttons"] = [{"status": "RELEASED", "id": "BOOT"}]
+            data_out["buttons"] = [{"status": "RELEASED", "id": button_id}]
     if data_out:
         print(json.dumps(data_out))
+
     time.sleep(0.1)
