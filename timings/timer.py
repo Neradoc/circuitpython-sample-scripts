ba40ec84c85a2409710b0392fb134d4aaa4f9987
Neradoc <neraOnGit@ri1.fr>
2021-10-04 18:14:56 +0200

    rework timer class to use decorators

--- timings/timer.py
+++ timings/timer.py
@@ -1,3 +1,12 @@
+"""
+A Timer class that will trigger a callback if the time has passed
+by calling Timer.update() in a loop.
+
+Rather than use independent timers, you can use module functions to manage
+a list of timers, and update them all in one call. Timers are automatically
+sorted by next run time to exit early from the update loop.
+"""
+
 import time
 
 if hasattr(time,"monotonic_ns"):
@@ -10,39 +19,76 @@ else:
     time_deconvert = lambda t: t
 
 class Timer:
-    def __init__(self, callback, delay, initial_delay=0, data=()):
+    def __init__(self, callback, delay, initial=None, data=()):
         now = monotonic()
         self.callback = callback
         self.delay = time_convert(delay)
-        self.initial_delay = time_convert(initial_delay)
+        if initial is None:
+            self.initial_delay = self.delay
+        else:
+            self.initial_delay = time_convert(initial)
         self.data = data
         self.next_run = now + self.initial_delay
+
+    def restart(self):
+        now = monotonic()
+        if self.initial_delay == 0:
+            self.run()
+        else:
+            self.next_run = now + self.initial_delay
+
+    def run(self):
+        self.callback(time_deconvert(self.next_run), self.data)
+        self.next_run += self.delay
 
     def update(self):
         if monotonic() > self.next_run:
             self.run()
 
-    def run(self):
-        self.callback(time_deconvert(self.next_run), self.data)
-        self.next_run += self.delay
-        
-
 all_timers = []
 
-def add_timer(*args, **kwargs):
-    t = Timer(*args, **kwargs)
-    all_timers.append(t)
-    all_timers.sort(key=lambda t: t.next_run)
-    return t
+def add_timer(delay, initial=None, data=()):
+    """
+    Add a timer with a decorator.
+    If initial is 0 (default) it will fire once the first time.
+    Otherwise it will wait that amount of time, regardless of the delay,
+    to start running every delay seconds.
+    If initial is None, the delay value is used as the initial delay.
+
+    :param delay: float number of seconds between runs of the timer.
+    :param initial: float number of seconds before the first run (0).
+
+    Example:
+
+        @timer.add_timer(TIMER_1_DELAY, data=1)
+        def timer_1_callback(now, data):
+            print(f"TIMER {data} --> {now:.2f}")
+    """
+    def _inside(callback):
+        t = Timer(callback, delay, initial, data)
+        all_timers.append(t)
+        all_timers.sort(key=lambda t: t.next_run)
+        return t
+    return _inside
+
+def restart_timer(timer, initial=0):
+    """Restart a timer that was stopped"""
+    if timer not in all_timer:
+        all_timers.append(t)
+        all_timers.sort(key=lambda t: t.next_run)
 
 def stop_timer(timer):
-    all_timers.remove(timer)
+    """Stop a timer instance from running."""
+    if timer in all_timers:
+        all_timers.remove(timer)
 
-def update():
+def stop_all():
+    """Stop all timers."""
+    all_timers = []
+
+def update_all():
     now = monotonic()
-    for timer in all_timers:
-        if now > timer.next_run:
-            timer.run()
-        else:
-            break
+    run = filter(lambda t: now > t.next_run, all_timers)
+    for timer in run:
+        timer.run()
     all_timers.sort(key=lambda t: t.next_run)
--- timings/timer_example.py
+++ timings/timer_example.py
@@ -6,16 +6,17 @@ TIMER_1_DELAY = 5
 # every 2 seconds:
 TIMER_2_DELAY = 2
 
+# add a timer with a decorator
+@timer.add_timer(TIMER_1_DELAY, data=1)
 def timer_1_callback(now, data):
-    print(f"TIMER {data} TRIGGERED {now:f}")
+    print(f"TIMER {data} --> {now:.2f}")
 
+# add a timer with a function call
 def timer_2_callback(now, data):
-    print(f"TIMER {data} TRIGGERED {now:f}")
-
-timer_1 = timer.add_timer(timer_1_callback, TIMER_1_DELAY, data=(1))
-timer_2 = timer.add_timer(timer_2_callback, TIMER_2_DELAY, data=(2))
+    print(f"TIMER {data} -->       {now:.2f}")
+timer_2 = timer.add_timer(TIMER_2_DELAY, data=2, initial=0)(timer_2_callback)
 
 while True:
-    timer.update()
+    timer.update_all()
     # sleep a little bit 
     time.sleep(0.01)
