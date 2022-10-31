e17453202b03eeb4e30eee9058189c8508879fe5
Neradoc <neraOnGit@ri1.fr>
2022-10-31 21:44:51 +0100

    configuration constants better at the top, Pull automatic

--- led_animations/chase_or_comet.py
+++ led_animations/chase_or_comet.py
@@ -3,7 +3,7 @@ Example of how to switch between animations
 Requires adafruit_led_animation
 """
 import board
-import digitalio
+from digitalio import DigitalInOut, Pull
 import time
 
 from adafruit_led_animation.animation.chase import Chase
@@ -12,15 +12,21 @@ from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.color import AMBER, JADE
 
 # Update to match the pin connected to your NeoPixels
-pixel_pin = board.A3
+PIXELS_PIN = board.A3
 # Update to match the number of NeoPixels you have connected
-pixel_num = 30
+PIXELS_NUM = 30
 # Update to match the button's pin
-# If the Pull direction had to be changed, change the button tests too
-button = digitalio.DigitalInOut(board.D6)
-button.switch_to_input(digitalio.Pull.UP)
+BUTTON_PIN = board.D6
+# Update to match the button value when pressed
+BUTTON_ON_VALUE = False
 
-pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.5, auto_write=False)
+button = DigitalInOut(BUTTON_PIN)
+button.switch_to_input(Pull.DOWN if BUTTON_ON_VALUE else Pull.UP)
+
+def button_pressed():
+	return button.value == BUTTON_ON_VALUE
+
+pixels = neopixel.NeoPixel(PIXELS_PIN, PIXELS_NUM, brightness=0.5, auto_write=False)
 
 chase = Chase(cp.pixels, speed=0.1, size=3, spacing=6, color=AMBER)
 comet = Comet(cp.pixels, speed=0.02, color=JADE, tail_length=10, bounce=True)
@@ -30,10 +36,10 @@ animations = AnimationSequence(comet, chase)
 while True:
 	animations.animate()
 	# lookup buttons to switch between animations
-	if not button.value:
+	if button_pressed():
 		animations.next()
 		# wait for button release (kind of debounce)
-		while not button.value:
+		while button_pressed():
 			time.sleep(0.01)
 	# for slow animations, sleep a bit, but not too long to read buttons
 	# time.sleep(min(0.1, anim.speed / 10))
