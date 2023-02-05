b615e9b2d61a9e8d9406f827a281302ea55f1812
Neradoc <Neradoc@users.noreply.github.com>
2023-02-05 16:46:01 +0100

    simplify Nina example, include offset

--- ntp_simple_demo/ntp_demo_nina.py
+++ ntp_simple_demo/ntp_demo_nina.py
@@ -3,25 +3,28 @@ Modified from https://gist.github.com/netroy/d1bff654fa58b884d63894ca2c890fc6
 """
 import board
 import rtc
-import struct
 import time
-import displayio
-import terminalio
-from adafruit_display_text import bitmap_label as label
 
-# configure your timezone, DST included
+#######################################################################
+# Configure your timezone, DST included
+#######################################################################
 TZ_OFFSET = 3600 * 2
 
-
-# SETUP ESP HERE
+#######################################################################
+# Setuo ESP here with your pins
+#######################################################################
 from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+spi = board.SPI()
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+#######################################################################
+# Connect to wifi
+#######################################################################
 
 print("CONNECT WIFI")
 while not esp.is_connected:
@@ -31,59 +34,41 @@ while not esp.is_connected:
 		print("could not connect to AP, retrying: ", e)
 		continue
 
-def get_ntp_time(esp):
-    # get_time will raise ValueError if the time isn't available yet so loop until
-    # it works.
-    now_utc = None
-    while now_utc is None:
-        try:
-            now_utc = time.localtime(esp.get_time()[0])
-        except OSError:
-            pass
-    rtc.RTC().datetime = now_utc
+#######################################################################
+# Time setting helper
+#######################################################################
 
-now = get_ntp_time(esp)
+def try_set_time(esp, tz_offset=0):
+    # get_time will raise ValueError if the time isn't available yet.
+    now = None
+    try:
+        now_utc = time.localtime(esp.get_time()[0] + tz_offset)
+    except OSError:
+        return False
+    rtc.RTC().datetime = now
+    return True
+
+#######################################################################
+# Set the time, waiting until it's available
+#######################################################################
+
+while not try_set_time(esp, TZ_OFFSET):
+    time.sleep(0.01)
+
 print("Synced with NTP")
-print(now)
 
-time.sleep(1.0)
+#######################################################################
+# Simple time formatting helper
+#######################################################################
 
-display = None
-if hasattr(board, "DISPLAY"):
-    display = board.DISPLAY
-else:
-    pass
-    # insert external display init
-
-if display:
-    display.auto_refresh = False
-    group = displayio.Group()
-    display.show(group)
-    text_area = label.Label(
-        terminalio.FONT,
-        scale=3,
-        color=(255, 255, 255),
-        anchor_point=(0.5, 0.5),
-        anchored_position=(
-            display.width // 2,
-            display.height // 2,
-        ),
-        text="Hello",
+def format_time(now):
+    return "{hour}:{minutes:02}:{seconds:02}".format(
+        hours=now.tm_hour, minutes=now.tm_min, seconds=now.tm_sec
     )
-    group.append(text_area)
-    display.refresh()
 
-previous_clock = ""
+#######################################################################
+# Print time
+#######################################################################
 
-while True:
-    now = time.localtime()
-    clock = "{hour:02d}:{min:02d}:{seconds:02d}".format(
-        hour=now.tm_hour, min=now.tm_min, seconds=now.tm_sec
-    )
-    if clock != previous_clock:
-        print(clock)
-        if display:
-            text_area.text = clock
-            display.refresh()
-        previous_clock = clock
-    time.sleep(0.2)
+now = time.localtime()
+print(format_time(now))
